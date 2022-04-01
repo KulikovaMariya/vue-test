a2ad2ab374d17cb92e694019f7e4f880b2bf1e77
Maria Kulikova <m.kulikova@aeronavigator.ru>
2022-04-01 13:23:45 +0300

    fix: getUserByUUID implementation

--- src/store/helpers.js
+++ src/store/helpers.js
@@ -2,8 +2,9 @@ export function getUserByUUID (users, UUID) {
   for (let i = 0; i < users.length; i++) {
     if (users[i].UUID === UUID) {
       return users[i]
-    } else if (users[i].subordinates && users[i].subordinates.length > 0) {
-      return  getUserByUUID(users[i].subordinates, UUID)
+    }
+    if (users[i].subordinates && users[i].subordinates.length > 0) {
+      getUserByUUID(users[i].subordinates, UUID)
     }
   }
 }
